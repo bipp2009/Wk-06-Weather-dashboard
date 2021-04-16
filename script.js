2a41983533adcb6f683401161dc40ce0dfc43557
Bipin <bipp2009@gmail.com>
2021-04-16 18:26:05 +0100

    currrent weather API data returns longitude and latitude, temperature, humidity, wind speed

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
 var APIKey = "9089f70eaa43a7727a45c93e56a716e3";
 
 $("#search-weather").on("click", function () {
-  console.log("Clicked");
+ 
   var city = $("#city-search-box").val()
-  console.log(city);
+ 
+  var currentWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey ; 
+  fetch(currentWeatherUrl).then(function(result) {
+    return result.json();
+  }).then(function(data){
+    console.log(data)
+    console.log("cityname:", data.name)
+    console.log("longitude", data.coord.lon)
+    //latitude
+    console.log("latitude", data.coord.lat)
+    //temp
+    console.log("temperature", data.main.temp)
+
+    //humidity
+    console.log("humidity", data.main.humidity)
+    //wind spped
+    console.log("wind speed", data.wind.speed)
+  })
+  var futureWeatherURl = "https://api.openweathermap.org/data/2.5/onecall?q=" + latitude + longitude "&appid=" + API key"
+  fetch(futureWeatherURl).then(function(result) {
+    return result.json();
+  }
+  }).then(function(data{
+    console.log(data)
+console.log("UVIndex"),
+  })
 });
 
 //https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={9089f70eaa43a7727a45c93e56a716e}
