6ccdf6e88b78e1ddc7ccdfd24a81a3c28a395ed2
Bipin <bipp2009@gmail.com>
2021-04-23 13:09:34 +0100

    JS code cleaned up and future get weather forecast function called to retrieve 5 day weather forecast

--- script.js
+++ script.js
@@ -1,34 +1,54 @@
-var APIKey = "9089f70eaa43a7727a45c93e56a716e3";
+let APIKey = "9089f70eaa43a7727a45c93e56a716e3";
 
-$("#search-weather").on("click", function () {
- 
-  var city = $("#city-search-box").val()
- 
-  var currentWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey ; 
-  fetch(currentWeatherUrl).then(function(result) {
-    return result.json();
-  }).then(function(data){
-    console.log(data)
-    console.log("cityname:", data.name)
-    console.log("longitude", data.coord.lon)
-    //latitude
-    console.log("latitude", data.coord.lat)
-    //temp
-    console.log("temperature", data.main.temp)
+function getWeather(city, processData) {
+  let currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
+  weatherData = fetch(currentWeatherUrl)
+    .then(function (result) {
+      return result.json();
+    })
+    .then(function (data) {
+      processData(data);
+    });
+}
 
-    //humidity
-    console.log("humidity", data.main.humidity)
-    //wind spped
-    console.log("wind speed", data.wind.speed)
-    var futureWeatherURl = "https://api.openweathermap.org/data/2.5/onecall?q=" + data.coord.lat + data.coord.lon "&appid=" + API key"
-  fetch(futureWeatherURl).then(function(result) {
-    return result.json();
-  }).then(function(data){
-    console.log(data)
-, console.log("UVIndex"),
-  })
-  })
-  
+function getForecast(lat, lon, processData) {
+  var futureWeatherURl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}&units=metric`;
+  weatherData = fetch(futureWeatherURl)
+    .then(function (result) {
+      return result.json();
+    })
+    .then(function (data) {
+      processData(data);
+    });
+}
+
+let weatherData;
+
+function showForecast(data) {
+  console.log("showForecast", data);
+}
+
+function showWeatherData(data) {
+  console.log("cityname:", data.name);
+
+  //temp
+  console.log("temperature", data.main.temp);
+
+  //humidity
+  console.log("humidity", data.main.humidity);
+  //wind spped
+  console.log("wind speed", data.wind.speed);
+
+  getForecast(data.coord.lat, data.coord.lon, showForecast);
+}
+
+$(document).ready(function () {
+  $("#search-weather").on("click", (e) => {
+    let city = $("#city-search-box").val();
+    getWeather(city, showWeatherData);
+  });
+
+  $("#show-long").on("click", (e) => {
+    alert(weatherData.coord.lon);
+  });
 });
-
-//https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={9089f70eaa43a7727a45c93e56a716e}
