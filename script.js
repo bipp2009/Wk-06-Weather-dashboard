04ebb3d8a4fad855c4486925bd31378d05b45f3f
Bipin <bipp2009@gmail.com>
2021-04-23 18:51:17 +0100

    Tided up code

--- script.js
+++ script.js
@@ -1,54 +1,51 @@
 let APIKey = "9089f70eaa43a7727a45c93e56a716e3";
 
-function getWeather(city, processData) {
+
+//Two functions to get the current weather and the future forecast
+function getWeather(city) {
   let currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
   weatherData = fetch(currentWeatherUrl)
     .then(function (result) {
       return result.json();
     })
     .then(function (data) {
-      processData(data);
+      showCurrent(data);
     });
 }
 
-function getForecast(lat, lon, processData) {
-  var futureWeatherURl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}&units=metric`;
+function getForecast(lat, lon) {
+  var futureWeatherURl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}&units=metricfuture `;
   weatherData = fetch(futureWeatherURl)
     .then(function (result) {
       return result.json();
     })
     .then(function (data) {
-      processData(data);
+      showForecast(data);
     });
 }
 
-let weatherData;
 
-function showForecast(data) {
-  console.log("showForecast", data);
-}
-
-function showWeatherData(data) {
+//Two functions to display the current weather and the future forecast on the screen
+function showCurrent(data) {
   console.log("cityname:", data.name);
-
   //temp
   console.log("temperature", data.main.temp);
-
   //humidity
   console.log("humidity", data.main.humidity);
   //wind spped
   console.log("wind speed", data.wind.speed);
 
-  getForecast(data.coord.lat, data.coord.lon, showForecast);
+  getForecast(data.coord.lat, data.coord.lon);
+}
+function showForecast(data) {
+  console.log("showForecast", data);
 }
 
+//Function to run the application search and everything else
 $(document).ready(function () {
   $("#search-weather").on("click", (e) => {
     let city = $("#city-search-box").val();
-    getWeather(city, showWeatherData);
+    getWeather(city);
   });
-
-  $("#show-long").on("click", (e) => {
-    alert(weatherData.coord.lon);
-  });
+ 
 });
